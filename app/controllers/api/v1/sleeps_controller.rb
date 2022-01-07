19f805aaaa0f2864fe168ad2995fe127bcb72ee9
Roald Ong <roald.ong@gmail.com>
2022-01-07 20:45:37 +0800

    Added in create API

--- app/controllers/api/v1/sleeps_controller.rb
+++ app/controllers/api/v1/sleeps_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::SleepsController < ApplicationController
+
+  skip_forgery_protection
   def index
     @all_sleeps= Sleeptime.all
     render json: @all_sleeps
   end
 
   def create
+    @new_sleep=Sleeptime.create!(check_params)
+    if @new_sleep
+      render json: @new_sleep
+    else render json: @new_sleep.errors
+    end
   end
 
   def find
@@ -12,4 +19,7 @@ class Api::V1::SleepsController < ApplicationController
 
   def delete
   end
+  def check_params
+        params.permit(:date, :hours)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'sleeps/index'
-      get 'sleeps/create'
+      post 'sleeps/create'
       get 'sleeps/find'
       get 'sleeps/delete'
     end
