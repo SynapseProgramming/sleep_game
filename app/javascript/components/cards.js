4d3f8d2547230159d4d96b1429c2d09808e04115
Roald Ong <roald.ong@gmail.com>
2022-01-07 22:06:44 +0800

    added in quality of sleep function

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -7,6 +7,22 @@ import Dnut from "./dnut";
 export default () => {
 	const [sleepData, setsleepData] = useState([]);
 
+	//function which returns an array, where the first ele is the no. of days the user slept for >=7 hrs
+	// the second ele is the no. of days the user slept for <7
+	const qos = inp => {
+		let good_sleep_cnt = 0;
+		let bad_sleep_cnt = 0;
+		inp.map(x => {
+			let curr = x["hours"];
+			if (curr <= 6) {
+				bad_sleep_cnt = bad_sleep_cnt + 1;
+			} else {
+				good_sleep_cnt = good_sleep_cnt + 1;
+			}
+		});
+		return [good_sleep_cnt, bad_sleep_cnt];
+	};
+
 	useEffect(() => {
 		const url = "/api/v1/sleeps/index";
 		let is_mounted = true;
@@ -21,6 +37,7 @@ export default () => {
 				.then(response => {
 					console.log(response);
 					setsleepData(response);
+					console.log(qos(response));
 				})
 				.catch(() => console.log("something went wrong!"));
 		}
