d8eb86e27eaa40335740abd4ce0ea5b8f87b55a7
Roald Ong <roald.ong@gmail.com>
2022-01-07 16:23:51 +0800

    Added in API to fetch all sleep data

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Card from "react-bootstrap/Card";
 import Graph from "./graph";
 
 export default () => {
+	useEffect(() => {
+		const url = "/api/v1/sleeps/index";
+		let is_mounted = true;
+		if (is_mounted) {
+			fetch(url)
+				.then(response => {
+					if (response.ok) {
+						return response.json();
+					}
+					throw new Error("Network response was not ok.");
+				})
+				.then(response => {
+					console.log(response);
+				})
+				.catch(() => console.log("something went wrong!"));
+		}
+		return () => {
+			is_mounted = false;
+		};
+	});
+
 	return (
 		<Card border="primary" className="text-center">
 			<Card.Header>Sleep Graph</Card.Header>
