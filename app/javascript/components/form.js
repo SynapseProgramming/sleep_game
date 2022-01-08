d28d60eae43f788248cd3627a800ca9a96f6949f
Roald Ong <roald.ong@gmail.com>
2022-01-08 11:49:00 +0800

    Successfully migrated to a date picker

--- app/javascript/components/form.js
+++ app/javascript/components/form.js
@@ -12,18 +12,7 @@ const schema = Yup.object({
 		.required("Required")
 		.max(24, "you can't sleep for more than 24 hrs in a day!")
 		.min(1, "0 is no sleep. this is not possible."),
-	year: Yup.number()
-		.required("Required")
-		.max(9999, "CIV may not even be around by then")
-		.min(0, "This is the start of civ"),
-	month: Yup.number()
-		.required("Required")
-		.max(12, "Only till dec")
-		.min(1, "Jan"),
-	day: Yup.number()
-		.required("Required")
-		.max(31, "31 Days in a month")
-		.min(1, "First Day")
+	date: Yup.string().required("Required")
 });
 
 const TaskInput = () => {
@@ -31,51 +20,56 @@ const TaskInput = () => {
 	const [failure, setFailure] = useState(false);
 	const [formRef, setFormRef] = useState(0);
 
+	// const updateDatabase = values => {
+	// 	const {hours, year, month, day} = values;
+	// 	const date = year + "-" + month + "-" + day;
+	// 	//simple json display
+	// 	const body = {
+	// 		hours,
+	// 		date
+	// 	};
+	// 	const token = document.querySelector('meta[name="csrf-token"]').content;
+	// 	const url = "api/v1/sleeps/create";
+	// 	fetch(url, {
+	// 		method: "POST",
+	// 		headers: {
+	// 			"X-CSRF-Token": token,
+	// 			"Content-Type": "application/json"
+	// 		},
+	// 		body: JSON.stringify(body)
+	// 	})
+	// 		.then(response => {
+	// 			if (response.ok) {
+	// 				return response.json();
+	// 			}
+	// 			throw new Error("Network response was not ok.");
+	// 		})
+	// 		.then(response => {
+	// 			setSuccess(true);
+	// 			ReactDOM.findDOMNode(formRef).reset();
+	// 			window.setTimeout(() => {
+	// 				setSuccess(false);
+	// 			}, 3000);
+	// 		})
+	// 		.catch(error => {
+	// 			console.log(error.message);
+	// 			setFailure(true);
+	// 			window.setTimeout(() => {
+	// 				setFailure(false);
+	// 			}, 6000);
+	// 		});
+	// };
+	//
+
 	const updateDatabase = values => {
-		const {hours, year, month, day} = values;
-		const date = year + "-" + month + "-" + day;
-		//simple json display
-		const body = {
-			hours,
-			date
-		};
-		const token = document.querySelector('meta[name="csrf-token"]').content;
-		const url = "api/v1/sleeps/create";
-		fetch(url, {
-			method: "POST",
-			headers: {
-				"X-CSRF-Token": token,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(body)
-		})
-			.then(response => {
-				if (response.ok) {
-					return response.json();
-				}
-				throw new Error("Network response was not ok.");
-			})
-			.then(response => {
-				setSuccess(true);
-				ReactDOM.findDOMNode(formRef).reset();
-				window.setTimeout(() => {
-					setSuccess(false);
-				}, 3000);
-			})
-			.catch(error => {
-				console.log(error.message);
-				setFailure(true);
-				window.setTimeout(() => {
-					setFailure(false);
-				}, 6000);
-			});
+		console.log(values);
 	};
 
 	const hehe = values => {
 		console.log(values);
 	};
 	const formik = useFormik({
-		initialValues: {hours: "", year: "", month: "", day: ""},
+		initialValues: {hours: "", date: ""},
 		onSubmit: updateDatabase,
 		validationSchema: schema
 	});
@@ -93,7 +87,6 @@ const TaskInput = () => {
 			<Alert show={failure} variant="danger">
 				Error! Task was not added to the database!
 			</Alert>
-			<Form.Control type="date" name="date_of_birth" />
 			<Form.Group className="mb-3" controlId="TaskInput">
 				<Form.Label>Hours Slept</Form.Label>
 				<Form.Control
@@ -109,51 +102,22 @@ const TaskInput = () => {
 					{formik.errors.hours}
 				</Form.Control.Feedback>
 			</Form.Group>
-			<Form.Group className="mb-3" controlId="YearInput">
-				<Form.Label>Enter Year</Form.Label>
+			<Form.Group className="mb-3" controlId="DateInput">
+				<Form.Label>Date</Form.Label>
 				<Form.Control
-					type="textarea"
-					placeholder="YYYY"
-					rows={3}
-					name="year"
+					type="date"
+					name="date"
 					onChange={formik.handleChange}
 					onBlur={formik.handleBlur}
-					isInvalid={!!formik.errors.year}
+					isInvalid={!!formik.errors.date}
 				/>
-				<Form.Control.Feedback type="invalid">
-					{formik.errors.year}
-				</Form.Control.Feedback>
-			</Form.Group>
-
-			<Form.Group className="mb-3" controlId="Month">
-				<Form.Label>Enter Month</Form.Label>
-				<Form.Control
-					type="textarea"
-					placeholder="MM"
-					rows={3}
-					name="month"
+				<Form.Control.Feedback
+					type="invalid"
 					onChange={formik.handleChange}
 					onBlur={formik.handleBlur}
-					isInvalid={!!formik.errors.month}
-				/>
-				<Form.Control.Feedback type="invalid">
-					{formik.errors.month}
-				</Form.Control.Feedback>
-			</Form.Group>
-
-			<Form.Group className="mb-3" controlId="Day">
-				<Form.Label>Enter Day</Form.Label>
-				<Form.Control
-					type="textarea"
-					placeholder="DD"
-					rows={3}
-					name="day"
-					onChange={formik.handleChange}
-					onBlur={formik.handleBlur}
-					isInvalid={!!formik.errors.day}
-				/>
-				<Form.Control.Feedback type="invalid">
-					{formik.errors.day}
+					isInvalid={!!formik.errors.date}
+				>
+					{formik.errors.date}
 				</Form.Control.Feedback>
 			</Form.Group>
 
